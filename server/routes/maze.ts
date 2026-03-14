9555d17c39b7e54a136681278d5f493e796d4001
aqwertyuiop48 <aqwertyuiop48@outlook.com>
2026-03-14 17:49:34 +0530

    Update internal URLs

--- server/routes/maze.ts
+++ server/routes/maze.ts
@@ -734,7 +734,7 @@ export default defineEventHandler(() =>
       };
       sprite = new Image();
       sprite.src =
-        "https://image.ibb.co/dr1HZy/Pf_RWr3_X_Imgur.png" +
+        "https://i.imgur.com/PfRWr3X.png" +
         "?" +
         new Date().getTime();
       sprite.setAttribute("crossOrigin", " ");
@@ -746,7 +746,7 @@ export default defineEventHandler(() =>
       };
     
       finishSprite = new Image();
-      finishSprite.src = "https://image.ibb.co/b9wqnJ/i_Q7m_U25_Imgur.png"+
+      finishSprite.src = "https://i.imgur.com/iQ7mU25.png"+
       "?" +
       new Date().getTime();
       finishSprite.setAttribute("crossOrigin", " ");
